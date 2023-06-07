5fce499ef55fcf52ba229929c42197efc50209d2
zacsanter <53116184+zacsanter@users.noreply.github.com>
2023-06-07 11:34:21 +0100

    Update animationscript.js

--- animationscript.js
+++ animationscript.js
@@ -1,38 +1,38 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    const elements = document.querySelectorAll('.reveal');
+window.addEventListener('load', function() {
+  const elements = document.querySelectorAll('.reveal');
 
-    elements.forEach(element => {
-      element.classList.remove('reveal');
-    });
+  elements.forEach(element => {
+    element.classList.remove('reveal');
   });
-  
-  function isInViewport(element) {
-    const bounding = element.getBoundingClientRect();
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-    const threshold = windowHeight * 0.06; // 6% of the viewport height
+  });
+ function isInViewport(element) {
+      const bounding = element.getBoundingClientRect();
+      const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+      const threshold = windowHeight * 0.06; // 6% of the viewport height
 
-    return (
-      bounding.top + threshold <= windowHeight &&
-      bounding.bottom >= threshold
-    );
-  }
+      return (
+        bounding.top + threshold <= windowHeight &&
+        bounding.bottom >= threshold
+      );
+    }
 
-  function handleViewportAnimations() {
-    const elements = document.querySelectorAll('.fadein, .fadeup, .fadedown, .fadeleft, .faderight, .scalein');
+    function handleViewportAnimations() {
+      const elements = document.querySelectorAll('.fadein, .fadeup, .fadedown, .fadeleft, .faderight, .scalein');
 
-    elements.forEach(element => {
-      if (isInViewport(element)) {
-        element.classList.add('in-viewport');
-      }
-    });
-  }
+      elements.forEach(element => {
+        if (isInViewport(element)) {
+          element.classList.add('in-viewport');
+        }
+      });
+    }
 
-  function init() {
-    // Initial check for elements in viewport
-    handleViewportAnimations();
+    function init() {
+      // Initial check for elements in viewport
+      handleViewportAnimations();
 
-    // Add event listener to check for elements in viewport on scroll
-    window.addEventListener('scroll', handleViewportAnimations);
-  }
+      // Add event listener to check for elements in viewport on scroll
+      window.addEventListener('scroll', handleViewportAnimations);
+    }
 
-  init();
+    init();
+ 
